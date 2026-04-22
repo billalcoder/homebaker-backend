a704f6b855404cfcf922dd27e5359913940a99bc
billalcoder <billalboys@gmail.com>
2026-04-22 16:44:36 +0530

    added secret

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,14 +1,19 @@
 module.exports = {
   apps: [
     {
-      name: "bakerlane", // This name will match your GH Variable PM2_NAME
+      name: "bakerlane",           // prod → pm2 list shows this
       script: "./app.js",
-      // This tells PM2 to look for the .env file in the folder it's running in
-      env_file: ".env", 
+      node_args: "--env-file=.env",
       env: {
-        NODE_ENV: "development",
-        // Manually add one key here just to test
-        RZP_KEY_ID: "rzp_live_your_actual_id1212" 
+        NODE_ENV: "production"
+      }
+    },
+    {
+      name: "bakerlane-test",      // dev → pm2 list shows this
+      script: "./app.js",
+      node_args: "--env-file=.env",
+      env: {
+        NODE_ENV: "development"
       }
     }
   ]
